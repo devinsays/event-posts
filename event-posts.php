c0c372725e62087d1fbc9564d69ac52e54e66c79
Devin Price <devin@wptheming.com>
2011-11-05 16:34:15 -0500

    Initial commit.

---
+++ event-posts.php
@@ -0,0 +1,215 @@
+<?
+/*
+Plugin Name: Sample Event Post Type
+Plugin URI: http://www.wptheming.com
+Description: Creates a custom post type for events with associated metaboxes.
+Version: 0.1
+Author: Devin Price
+Author URI: http://www.wptheming.com
+License: GPLv2 or later
+*/
+
+/**
+ * Flushes rewrite rules on plugin activation to ensure event posts don't 404
+ * http://codex.wordpress.org/Function_Reference/flush_rewrite_rules
+ */
+
+function ep_eventposts_activation() {
+	ep_eventposts();
+	flush_rewrite_rules();
+}
+
+register_activation_hook( __FILE__, 'ep_eventposts_activation' );
+
+function ep_eventposts() {
+
+	/**
+	 * Enable the event custom post type
+	 * http://codex.wordpress.org/Function_Reference/register_post_type
+	 */
+
+	$labels = array(
+		'name' => __( 'Events', 'eventposttype' ),
+		'singular_name' => __( 'Event', 'eventposttype' ),
+		'add_new' => __( 'Add New Event', 'eventposttype' ),
+		'add_new_item' => __( 'Add New Event', 'eventposttype' ),
+		'edit_item' => __( 'Edit Event', 'eventposttype' ),
+		'new_item' => __( 'Add New Event', 'eventposttype' ),
+		'view_item' => __( 'View Event', 'eventposttype' ),
+		'search_items' => __( 'Search Events', 'eventposttype' ),
+		'not_found' => __( 'No events found', 'eventposttype' ),
+		'not_found_in_trash' => __( 'No events found in trash', 'eventposttype' )
+	);
+
+	$args = array(
+    	'labels' => $labels,
+    	'public' => true,
+		'supports' => array( 'title', 'editor', 'thumbnail', 'comments' ),
+		'capability_type' => 'post',
+		'rewrite' => array("slug" => "event"), // Permalinks format
+		'menu_position' => 5,
+		'menu_icon' => plugin_dir_url( __FILE__ ) . '/images/calendar-icon.gif',  // Icon Path
+		'has_archive' => true
+	); 
+
+	register_post_type( 'event', $args );
+}
+
+add_action( 'init', 'ep_eventposts' );
+
+/**
+ * Adds event post metaboxes for start time and end time
+ * http://codex.wordpress.org/Function_Reference/add_meta_box
+ */
+
+function ep_eventposts_metaboxes() {
+	// We want two time event metaboxes, one for the start time and one for the end time
+	// Two avoid repeating code, we'll just pass the $identifier in a callback
+	$start = array( 'id' => '_start');
+	$end = array('id'=>'_end');
+	// If you wanted to add this to regular posts instead, just swap 'event' for 'post' below
+	add_meta_box( 'ept_event_date_start', 'Start Date and Time', 'ept_event_date', 'event', 'side', 'default', $start );
+	add_meta_box( 'ept_event_date_end', 'End Date and Time', 'ept_event_date', 'event', 'side', 'default', $end );
+}
+add_action( 'admin_init', 'ep_eventposts_metaboxes' );
+
+// Metabox HTML
+
+function ept_event_date($post, $args) {
+	$metabox_id = $args['args']['id'];
+	global $post, $wp_locale;
+
+	// Use nonce for verification
+	wp_nonce_field( plugin_basename( __FILE__ ), 'ep_eventposts_nonce' );
+
+	$time_adj = current_time( 'timestamp' );
+	$month = get_post_meta( $post->ID, $metabox_id . '_month', true );
+
+	if ( empty( $month ) ) {
+		$month = gmdate( 'm', $time_adj );
+	}
+
+	$day = get_post_meta( $post->ID, $metabox_id . '_day', true );
+
+	if ( empty( $day ) ) {
+		$day = gmdate( 'd', $time_adj );
+	}
+
+	$year = get_post_meta( $post->ID, $metabox_id . '_year', true );
+
+	if ( empty( $year ) ) {
+		$year = gmdate( 'Y', $time_adj );
+	}
+	
+	$hour = get_post_meta($post->ID, $metabox_id . '_hour', true);
+ 
+    if ( empty($hour) ) {
+        $hour = gmdate( 'H', $time_adj );
+    }
+ 
+    $min = get_post_meta($post->ID, $metabox_id . '_minute', true);
+ 
+    if ( empty($min) ) {
+        $min = '00';
+    }
+
+	$month_s = '<select name="' . $metabox_id . '_month">';
+	for ( $i = 1; $i < 13; $i = $i +1 ) {
+		$month_s .= "\t\t\t" . '<option value="' . zeroise( $i, 2 ) . '"';
+		if ( $i == $month )
+			$month_s .= ' selected="selected"';
+		$month_s .= '>' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) . "</option>\n";
+	}
+	$month_s .= '</select>';
+
+	echo $month_s;
+	echo '<input type="text" name="' . $metabox_id . '_day" value="' . $day  . '" size="2" maxlength="2" />';
+    echo '<input type="text" name="' . $metabox_id . '_year" value="' . $year . '" size="2" maxlength="2" /> @ ';
+    echo '<input type="text" name="' . $metabox_id . '_hour" value="' . $hour . '" size="2" maxlength="2"/>:';
+    echo '<input type="text" name="' . $metabox_id . '_minute" value="' . $min . '" size="2" maxlength="2" />';
+ 
+}
+
+// Save the Metabox Data
+
+function ep_eventposts_save_meta( $post_id, $post ) {
+
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+		return;
+
+	if ( !isset( $_POST['ep_eventposts_nonce'] ) )
+		return;
+
+	if ( !wp_verify_nonce( $_POST['ep_eventposts_nonce'], plugin_basename( __FILE__ ) ) )
+		return;
+
+	// Is the user allowed to edit the post or page?
+	if ( !current_user_can( 'edit_post', $post->ID ) )
+		return;
+
+	// OK, we're authenticated: we need to find and save the data
+	// We'll put it into an array to make it easier to loop though
+	
+	$metabox_ids = array( '_start', '_end' );
+
+	foreach ($metabox_ids as $key ) {
+		$events_meta[$key . '_month'] = $_POST[$key . '_month'];
+	    $events_meta[$key . '_day'] = $_POST[$key . '_day'];
+	        if($_POST[$key . '_hour']<10){
+	             $events_meta[$key . '_hour'] = '0'.$_POST[$key . '_hour'];
+	         } else {
+	               $events_meta[$key . '_hour'] = $_POST[$key . '_hour'];
+	         }
+	    $events_meta[$key . '_year'] = $_POST[$key . '_year'];
+	    $events_meta[$key . '_hour'] = $_POST[$key . '_hour'];
+	    $events_meta[$key . '_minute'] = $_POST[$key . '_minute'];
+	    $events_meta[$key . '_eventtimestamp'] = $events_meta[$key . '_year'] . $events_meta[$key . '_month'] . $events_meta[$key . '_day'] . $events_meta[$key . '_hour'] . $events_meta[$key . '_minute'];
+    }
+ 
+
+	// Add values of $events_meta as custom fields
+
+	foreach ( $events_meta as $key => $value ) { // Cycle through the $events_meta array!
+		if ( $post->post_type == 'revision' ) return; // Don't store custom data twice
+		$value = implode( ',', (array)$value ); // If $value is an array, make it a CSV (unlikely)
+		if ( get_post_meta( $post->ID, $key, FALSE ) ) { // If the custom field already has a value
+			update_post_meta( $post->ID, $key, $value );
+		} else { // If the custom field doesn't have a value
+			add_post_meta( $post->ID, $key, $value );
+		}
+		if ( !$value ) delete_post_meta( $post->ID, $key ); // Delete if blank
+	}
+
+}
+
+add_action( 'save_post', 'ep_eventposts_save_meta', 1, 2 );
+
+/**
+ * Helpers to display the date on the front end
+ */
+
+// Get the Month Abbreviation
+ 
+function eventposttype_get_the_month_abbr($month) {
+    global $wp_locale;
+    for ( $i = 1; $i < 13; $i = $i +1 ) {
+                if ( $i == $month )
+                    $monthabbr = $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) );
+                }
+    return $monthabbr;
+}
+ 
+// Display the date
+ 
+function eventposttype_get_the_event_date() {
+    global $post;
+    $eventdate = '';
+    $month = get_post_meta($post->ID, '_month', true);
+    $eventdate = eventposttype_get_the_month_abbr($month);
+    $eventdate .= ' ' . get_post_meta($post->ID, '_day', true) . ',';
+    $eventdate .= ' ' . get_post_meta($post->ID, '_year', true);
+    $eventdate .= ' at ' . get_post_meta($post->ID, '_hour', true);
+    $eventdate .= ':' . get_post_meta($post->ID, '_minute', true);
+    echo $eventdate;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Event Posts ===
+Contributors: Devin Price
+Tags: events, post type
+Requires at least: 3.1
+Tested up to: 3.2
+Stable tag: 0.1
+License: GPLv2
+
+== Description ==
+
+This plugin registers a custom post type for events and displays the associated meta boxes.
+
+I'll follow up with a tutorial that explains how to query them.
+
+== Installation ==
+
+Just install and activate.
+
+== Frequently Asked Questions ==
+
+= How can I display events in my theme? =
+
+Working on it.
+
+== Changelog ==
+
+= 0.1 =
+
+* Initial release
