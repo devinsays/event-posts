7f6f8ba7da47ddd9bd7c0f03c87ce528d71cf978
Devin Price <devin@wptheming.com>
2011-11-20 16:41:53 -0600

    Update parameters for queries.

--- archive-event.php
+++ archive-event.php
@@ -11,7 +11,9 @@
 
 <?php
 	// http://codex.wordpress.org/Function_Reference/current_time
-	$current_time = current_time( 'timestamp' );
+	$current_time = current_time('mysql'); 
+	list( $today_year, $today_month, $today_day, $hour, $minute, $second ) = split( '([^0-9])', $current_time );
+	$current_timestamp = $today_year . $today_month . $today_day . $hour . $minute;
 ?>
 
 <h1>Event Archive Page</h1>
@@ -67,19 +69,43 @@
 		$paged = 1;
 		
 	// Arguments for the modified query
-	$args = array( 'post_type' => 'event',
+	$args = array(
+		'post_type' => 'event',
 		'meta_key' => '_start_eventtimestamp',
 		'orderby'=> 'meta_value_num',
+		'meta_value' => $current_timestamp,
+		'meta_compare' => '>=',
 		'order' => 'ASC',
 		'posts_per_page' => 20,
-		'paged' => $paged );
+		'paged' => $paged
+	);
 	$events = new WP_Query( $args );
 
 	if ( $events->have_posts() ) :
 		echo '<ul>';
-		while ( $events->have_posts() ) : $events->the_post();
-			echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
-		endwhile;
+		while ( $events->have_posts() ) : $events->the_post(); ?>
+			<li>
+			<h4><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
+			<ul>
+				<li>Publish date: <?php the_date('M d Y'); ?></li>
+				<?php 
+				// Gets the event start month from the meta field
+				$month = get_post_meta( $post->ID, '_start_month', true );
+				// Converts the month number to the month name
+				$month = $wp_locale->get_month_abbrev( $wp_locale->get_month( $month ) );
+				// Gets the event start day
+				$day = get_post_meta( $post->ID, '_start_day', true );
+				// Gets the event start year
+				$year = get_post_meta( $post->ID, '_start_year', true );
+				?>
+				
+				<li>Event start date: <?php echo $month . ' ' . $day . ' ' . $year; ?></li>
+				
+				<li>Meta _start_eventtimestamp: <?php echo get_post_meta( $post->ID, '_start_eventtimestamp', true ); ?></li>
+				<li>Current custom timestamp: <?php echo $current_timestamp; ?></li>
+			</ul>
+			</li>
+		<?php endwhile;
 		echo '</ul>';
 	endif;
 ?>
